b88336b2970e3048244e98d5579d8d5a2e73ae3d
Yangguang Li <liyangguangcn@gmail.com>
2019-12-06 22:16:15 -0800

    Course list page

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,13 +4,13 @@ main
   .grid
     ant-card(v-for="(item, _key) in courses", :key="_key", :title="item.course_name")
       ant-button(slot="extra", shape="circle", icon="edit", @click="edit(item)", aria-label="编辑课程")
-      p.teacher 老师: {{_getTeacher(item.teacher_pid).name || '(未找到)'}}
-      p.teacher 助教: {{_getTeacher(item.assistant_pid).name || '(未找到)'}}
+      p.teacher 老师: {{getTeacherName(item.teacher_pid)}}
+      p.teacher 助教: {{getTeacherName(item.assistant_pid)}}
       p.more 课程信息: {{item.course_intro}}
-        ant-button.student-button(size="small") 管理
+      ant-button(size="small") 管理学生
     ant-card(title="添加新课程", :bodyStyle="{'min-height': '10em'}")
       ant-button.add-button(type="primary", shape="circle", icon="plus", size="large", @click="addNew", aria-label="添加")
-  ant-modal(:title="modalContent.id ? '编辑课程' : '添加新课程'", :visible="isModalVisible", @ok="dialogOk", @cancel="isModalVisible = false")
+  ant-modal(:title="modalContent.pid ? '编辑课程' : '添加新课程'", :visible="isModalVisible", @ok="dialogOk", @cancel="isModalVisible = false")
     ant-form(formLayout="horizontal")
       ant-form-item(label="名称", :label-col="{span: 4}", :wrapper-col="{span: 20}"): ant-input(v-model="modalContent.course_name")
       ant-form-item(label="老师", :label-col="{span: 4}", :wrapper-col="{span: 20}")
@@ -59,13 +59,13 @@ export default {
     },
     _loadCourses() {
       return callApi('config/course').then((courses) => {
-        this.courses = courses.filter((course) => course.teacher_pid === this.$store.state.currentUserPid || course.assistant_pid === this.$store.state.currentUserPid);
+        this.courses = courses.filter((course) => course.teacher_pid === this.$store.state.currentUser.pid || course.assistant_pid === this.$store.state.currentUser.pid);
       }).catch((error) => {
         this.dialogMessage = error;
       });
     },
     addNew() {
-      this.modalContent = {};
+      this.modalContent = {institute_pid: this.$store.state.currentUser.institute_pid};
       this.isModalVisible = true;
     },
     edit(item) {
@@ -73,17 +73,21 @@ export default {
       this.isModalVisible = true;
     },
     dialogOk() {
-      if (!this.modalContent.course_uid) {
-        this.modalContent.course_uid = this.modalContent.course_name.toLowerCase().replace(/\W+/, '-');
-      }
-      return this._saveItem(this.modalContent).then((newPin) => {
-        this.modalContent.pin = newPin;
-        this.courses.push(this.modalContent);
+      const isNew = !this.modalContent.pid;
+      return this._saveItem(this.modalContent).then((newPid) => {
+        this.modalContent.pid = newPid;
+        if (isNew) {
+          this.courses.push(this.modalContent);
+        }
         this.isModalVisible = false;
       });
     },
-    _getTeacher(pid) {
-      return this.teacherList.filter((teacher) => teacher.pid === pid)[0] || {};
+    getTeacherName(pid) {
+      if (!this.teacherList.length) return '(载入中)';
+
+      const teacher = this.teacherList.filter((teacher) => teacher.pid === pid)[0];
+      if (!teacher) throw new Error(`No teacher found matching pin ${pid}`);
+      return teacher.teacher_name;
     },
     _saveItem(item) {
       return callApi('config/course', item, item.pid ? 'PUT' : 'POST').catch((error) => {
@@ -109,7 +113,7 @@ main {
 }
 
 p {
-  margin: .5em;
+  margin: .5em 0;
 }
 
 .student-button {
