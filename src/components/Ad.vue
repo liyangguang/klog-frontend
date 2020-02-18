854d9cfa1e23788f8f71f4ac2d5a931f028c2de7
Yangguang Li <liyangguangcn@gmail.com>
2020-02-17 21:41:58 -0800

    Add facebook tracking.

--- src/components/Ad.vue
+++ src/components/Ad.vue
@@ -34,11 +34,25 @@ export default {
     }
   },
   created() {
+    this._addFacebookTracking();
     setInterval(() => {
       this.nextPhoto();
-      }, SLIDE_INTERVAL);
+    }, SLIDE_INTERVAL);
+
   },
   methods: {
+    _addFacebookTracking(){
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window,document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+       fbq('init', '215864872930611'); 
+      fbq('track', 'PageView');
+    },
     nextPhoto(){
       this.photoIndex = (this.photoIndex + 1) % this.photos.length;
     },
